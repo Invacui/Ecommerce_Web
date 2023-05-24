8384d5d7a477b42265ff8f87b095e676a3e42328
Invacui <Ask.72544@gmail.com>
2023-05-25 00:53:03 +0530

    Hero Page Intigration

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Header from './components/Header';
+import Home from './moduels/Home';
 function App() {
   return (
+    
     <div className='text-grey-600'>
-    Hello World  
+    <Header/>
+    <Home/>
+
     </div>
   );
 }
+
 
 export default App;
