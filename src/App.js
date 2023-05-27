94cf549c751acdf2f8bc19617f057492b7717579
Invacui <Ask.72544@gmail.com>
2023-05-27 21:34:28 +0530

    Cart Add Init

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
 import Home from './moduels/Home';
+import Login from './moduels/LOGIN';
+import Footer from './components/Footer';
 function App() {
   return (
     
     <div className='text-grey-600'>
     <Header/>
     <Home/>
+    <Footer/>
     </div>
   );
 }
